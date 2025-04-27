7602dbf59fe3cd1dd7450d5b111c6067b843be4d
Heri Julianto <herijulianto22@gmail.com>
2025-04-27 19:14:26 +0700

    Perubahan tambahan pada barang baru

---
+++ database/migrations/2025_04_27_112850_create_barang_keluars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang_keluars', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_barang');
+            $table->integer('jumlah');
+            $table->string('tujuan');
+            $table->date('tanggalmasuk');
+            $table->timestamps();
+            $table->foreign('kode_barang')->references('kode_barang')->on('stoks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_keluars');
+    }
+};
